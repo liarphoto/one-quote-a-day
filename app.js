c74feafa47b7b3ac7329e5e3463465af7399bcbd
sonukomar <enggsonukomar@gmail.com>
2017-07-06 22:30:05 +0530

    updated package.json

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
 app.listen(3000,function(request, response){
     console.log("Express server is running on http://localhost:3000");
 });
@@ -8,5 +11,24 @@ app.listen(3000,function(request, response){
 app.get('/',function(request, response){
     response.sendFile(__dirname+'/index.html');
 });
+
+
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
 
 module.exports = app;
